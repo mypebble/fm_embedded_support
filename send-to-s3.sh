b84ff8608d31bfefd12f0245e82294fc9d590b04
Scott Walton <scott.walton@mypebble.co.uk>
2016-07-27 11:25:31 +0100

    Use the CLI argument

--- send-to-s3.sh
+++ send-to-s3.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-S3_DIR = $1
 ## Based on http://benlopatin.com/deploying-static-sites-circle-ci/
-aws s3 sync $SYNC_DIR s3://$S3_BUCKET/$S3_DIR/fm-embedded --delete --acl public-read --cache-control max-age=120
+aws s3 sync $SYNC_DIR s3://$S3_BUCKET/$1/fm-embedded --delete --acl public-read --cache-control max-age=120
