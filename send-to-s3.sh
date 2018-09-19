f0eeb91550ab46bd9c852a0a3445c48610334a18
Tom Picton <tom@tompicton.co.uk>
2018-09-19 11:05:46 +0100

    Add AWS auth to env

--- send-to-s3.sh
+++ send-to-s3.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ## Based on http://benlopatin.com/deploying-static-sites-circle-ci/
-aws s3 sync output s3://$S3_BUCKET/fm-embedded --acl public-read --cache-control max-age=120
+AWS_ACCESS_KEY_ID=$NEW_AWS_ID AWS_SECRET_ACCESS_KEY=$NEW_AWS_SECRET aws s3 sync output s3://$S3_BUCKET/fm-embedded --acl public-read --cache-control max-age=120
