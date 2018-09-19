59150ba4d073821aaa82812a4a2d105454b3da55
Tom Picton <tom@tompicton.co.uk>
2018-09-19 10:38:47 +0100

    Fix syntax error

--- send-to-s3.sh
+++ send-to-s3.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ## Based on http://benlopatin.com/deploying-static-sites-circle-ci/
-aws s3 sync output SYNC_DIR s3://$S3_BUCKET/fm-embedded --acl public-read --cache-control max-age=120
+aws s3 sync output s3://$S3_BUCKET/fm-embedded --acl public-read --cache-control max-age=120
